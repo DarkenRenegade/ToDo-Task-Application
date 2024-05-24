8c90168728dbe8e4f0fb4230b611e82be72babef
Clay Wright <gclaywright@gmail.com>
2024-05-24 14:30:03 -0700

    Task name validation added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 class Task {
 }
 let myTask = new Task();
-myTask.taskname = "Shop for groceries";
-myTask.iscomplete = true;
+myTask.taskName = "Shop for groceries";
+myTask.isComplete = true;
 console.log(myTask);
+window.onload = function () {
+    let addTaskBtn = document.querySelector("#add-task");
+    addTaskBtn.onclick = processTask;
+};
+function processTask() {
+    console.log("processTask was called");
+    let userTask = getTask();
+    if (userTask != null) {
+        addTask(userTask);
+    }
+}
+function getTask() {
+    let taskNameTextBox = document.querySelector("#taskname");
+    let isValidData = true;
+    let taskName = taskNameTextBox.value;
+    if (!isValidTaskName(taskName)) {
+        isValidData = false;
+        taskNameTextBox.nextElementSibling.textContent = "The name must be 1000 characters or less.";
+    }
+}
+function isValidTaskName(data) {
+    if (data.length > 1000) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+function addTask(t) { }
--- ts/main.ts
+++ ts/main.ts
@@ -5,17 +5,78 @@ class Task {
     /**
      * The name of the task
      */
-    taskname : string;
+    taskName : string;
 
     /**
      * Checks if the task is complete
      */
-    iscomplete : boolean;
+    isComplete : boolean;
 }
 
 // Task object test code
 let myTask = new Task();
-myTask.taskname = "Shop for groceries";
-myTask.iscomplete = true;
+myTask.taskName = "Shop for groceries";
+myTask.isComplete = true;
 
 console.log(myTask);
+
+
+window.onload = function() {
+    // Set up button click for add task form
+    let addTaskBtn:HTMLButtonElement = document.querySelector("#add-task") as HTMLButtonElement;
+    addTaskBtn.onclick = processTask;
+}
+
+function processTask() {
+    console.log("processTask was called");
+
+    let userTask = getTask();
+    if (userTask != null) {
+        addTask(userTask);
+    }
+}
+
+/**
+ * This function will retrieve all the task
+ * data from the HTML page. If said task is
+ * complete, a Task object will be returned.
+ * If there is any invalid data in a Task,
+ * null will be returned and error messages
+ * will be shown on the web page.
+ */
+function getTask():Task {
+    // Get the input
+    let taskNameTextBox = document.querySelector("#taskname") as HTMLInputElement;
+
+    // Validate data
+    let isValidData:boolean = true;
+
+    // Validate the task name
+    let taskName:string = taskNameTextBox.value;
+    if (!isValidTaskName(taskName)) {
+        isValidData = false;
+        taskNameTextBox.nextElementSibling.textContent = "The name must be 1000 characters or less."
+    }
+}
+
+/**
+ * This validates a task name. Returns true
+ * if the name consists of an expression pattern
+ * for valid task names.
+ * @param data The string to be validated
+ * @returns True if data is a valid task pattern
+ */
+function isValidTaskName(data:string) {
+    if (data.length > 1000) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+/**
+ * Adds a Task objext to web storage. Assumes
+ * all data is valid.
+ * @param t The Task containing valid data will be added
+ */
+function addTask(t:Task):void {}
